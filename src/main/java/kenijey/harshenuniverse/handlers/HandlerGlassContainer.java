386b9e69f6e5e6572d508e150be2637d2882362b
JanneSoon <41585837+JanneSoon@users.noreply.github.com>
2018-08-09 17:35:54 +0300

    removed obtaining glass container regen by holding it in the off hand on hitting entity

--- src/main/java/kenijey/harshenuniverse/handlers/HandlerGlassContainer.java
+++ src/main/java/kenijey/harshenuniverse/handlers/HandlerGlassContainer.java
@@ -2,31 +2,14 @@ package kenijey.harshenuniverse.handlers;
 
 import kenijey.harshenuniverse.HarshenItems;
 import kenijey.harshenuniverse.enums.items.GlassContainerValues;
-import kenijey.harshenuniverse.items.GlassContainer;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EntityDamageSource;
 import net.minecraft.util.EnumHand;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class HandlerGlassContainer 
 {
-	@SubscribeEvent
-	public void onEntityHit(LivingHurtEvent event)
-	{
-		if(event.getSource() instanceof EntityDamageSource && ((EntityDamageSource) event.getSource()).getTrueSource() instanceof EntityLivingBase && 
-				((EntityLivingBase)((EntityDamageSource) event.getSource()).getTrueSource()).getHeldItemMainhand().getItem() instanceof GlassContainer && 
-				((EntityLivingBase)((EntityDamageSource) event.getSource()).getTrueSource()).getHeldItemMainhand().getItemDamage() == 0)
-		{
-			ItemStack stack = ((EntityLivingBase)((EntityDamageSource) event.getSource()).getTrueSource()).getHeldItemMainhand();
-			stack.setItemDamage(2);
-			((EntityLivingBase)((EntityDamageSource) event.getSource()).getTrueSource()).setHeldItem(EnumHand.MAIN_HAND, stack);
-		}		
-	}
-	
 	@SubscribeEvent
 	public void onEntityRightClick(PlayerInteractEvent.EntityInteract event)
 	{
