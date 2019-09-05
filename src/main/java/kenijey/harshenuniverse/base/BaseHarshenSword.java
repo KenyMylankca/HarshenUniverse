9194dc9e4aa06543420e97778606475885f21bb2
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2019-09-05 16:22:43 +0300

    upgraded BaseHarshenSword to allow set repairing item easily and removed the base tooltip info

--- src/main/java/kenijey/harshenuniverse/base/BaseHarshenSword.java
+++ src/main/java/kenijey/harshenuniverse/base/BaseHarshenSword.java
@@ -1,12 +1,10 @@
 package kenijey.harshenuniverse.base;
 
-import java.util.List;
+import javax.annotation.Nullable;
 
-import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
 
 public abstract class BaseHarshenSword extends ItemSword
@@ -22,9 +20,10 @@ public abstract class BaseHarshenSword extends ItemSword
 	protected abstract String getName();
 	
 	@Override
-	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		String type = this.getName();
-		tooltip.add("\u00A73" + new TextComponentTranslation(type + "1").getFormattedText());
-		super.addInformation(stack, worldIn, tooltip, flagIn);
+	public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+		return repair.getItem() == getRepairItem();
 	}
+	
+	@Nullable
+	protected abstract Item getRepairItem();
 }
