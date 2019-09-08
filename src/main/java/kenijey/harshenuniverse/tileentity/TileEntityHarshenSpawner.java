f8b89dc748fabc21200bd882dfc503626de4c579
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2019-09-08 15:14:57 +0300

    improved the harshen spawner to can be deactivated. fix #25

--- src/main/java/kenijey/harshenuniverse/tileentity/TileEntityHarshenSpawner.java
+++ src/main/java/kenijey/harshenuniverse/tileentity/TileEntityHarshenSpawner.java
@@ -1,9 +1,10 @@
 package kenijey.harshenuniverse.tileentity;
 
+import java.util.UUID;
+
 import kenijey.harshenuniverse.HarshenSounds;
 import kenijey.harshenuniverse.base.BaseTileEntityHarshenSingleItemInventory;
 import kenijey.harshenuniverse.config.GeneralConfig;
-import kenijey.harshenuniverse.items.RitualStick;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
@@ -18,7 +19,9 @@ import net.minecraft.util.math.BlockPos;
 
 public class TileEntityHarshenSpawner extends BaseTileEntityHarshenSingleItemInventory
 {
-	private EntityLiving entityliving;
+	private EntityLiving entityliving = null;
+	private UUID spawnedEntityUUID = null;
+	private ItemStack spawnedEntitysEgg = null;
 	
 	public Entity getEntity(ItemStack stack)
 	{
@@ -31,6 +34,7 @@ public class TileEntityHarshenSpawner extends BaseTileEntityHarshenSingleItemInv
 		{
 			entityliving = (EntityLiving) EntityList.createEntityByIDFromName(ItemMonsterPlacer.getNamedIdFrom(stack), world);
             entityliving.onInitialSpawn(world.getDifficultyForLocation(new BlockPos(entityliving)), (IEntityLivingData)null);
+            spawnedEntitysEgg = stack;
 		}
 		catch (NullPointerException e) {
 		}
@@ -41,8 +45,18 @@ public class TileEntityHarshenSpawner extends BaseTileEntityHarshenSingleItemInv
 	protected void tick() 
 	{
 		EntityPlayer player = world.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), GeneralConfig.harshenSpawnerDistance, false);
-		if((player != null && getEntity(getItem()) != null) && (!player.capabilities.isCreativeMode || (player.capabilities.isCreativeMode && player.getHeldItemOffhand().getItem() instanceof RitualStick)))
+		if(player != null && getEntity(getItem()) != null && !player.isCreative())
 			activate(player);
+		
+		if(entityliving==null)
+			for(Entity entity : world.loadedEntityList)
+				if(entity.getUniqueID() == spawnedEntityUUID)
+				{
+					if(entityliving == null && !entity.isEntityAlive())
+						world.setBlockToAir(pos);
+					if(entity.isEntityAlive() && (player == null || player.isCreative()) && entityliving == null)
+						deactivate(spawnedEntitysEgg, entity);
+				}
 	}
 	
 	private void activate(EntityPlayer player)
@@ -53,7 +67,13 @@ public class TileEntityHarshenSpawner extends BaseTileEntityHarshenSingleItemInv
 		entityliving.onInitialSpawn(world.getDifficultyForLocation(new BlockPos(entityliving)), (IEntityLivingData)null);
 		if(!world.isRemote)
 			world.spawnEntity(this.entityliving);
+		spawnedEntityUUID = entityliving.getUniqueID();
 		world.playSound(pos.getX(), pos.getY(), pos.getZ(), HarshenSounds.SPAWNER_SUMMON, SoundCategory.BLOCKS, 1f, 1f, false);
-		world.setBlockToAir(pos);
+	}
+	
+	private void deactivate(ItemStack stack, Entity entity)
+	{
+		setItem(stack);
+		entity.setDead();
 	}
 }
