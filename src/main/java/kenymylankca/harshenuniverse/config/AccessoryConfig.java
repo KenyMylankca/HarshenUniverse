f768ab88cad5b73fb675d6503de3779820740462
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2020-10-17 20:01:20 +0300

    missing part of the last commit

--- src/main/java/kenymylankca/harshenuniverse/config/AccessoryConfig.java
+++ src/main/java/kenymylankca/harshenuniverse/config/AccessoryConfig.java
@@ -27,10 +27,10 @@ public class AccessoryConfig extends BaseConfig
 		xrayAreaX = get("xray_distance_x", HarshenItems.XRAY_PENDANT, 35);
 		xrayAreaY = get("xray_distance_y", HarshenItems.XRAY_PENDANT, 35);
 		xrayAreaZ = get("xray_distance_z", HarshenItems.XRAY_PENDANT, 35);
-		xrayListSize = get("xray_listsize", HarshenItems.XRAY_PENDANT, 20); 
+		xrayListSize = get("xray_list_size", HarshenItems.XRAY_PENDANT, 20); 
 		enderNecklaceDistance = get("ender_necklace_distance", HarshenItems.ENDER_NECKLACE, 66);
 		reachPendantLength = get("reach_pendant_length", HarshenItems.REACH_PENDANT, 7d);
-		punchyRingAttackDamage = get("punchy_ring_attack.damage", HarshenItems.PUNCHY_RING, 2.5);
+		punchyRingAttackDamage = get("punchy_ring_attack_damage", HarshenItems.PUNCHY_RING, 2.5);
 	}
 	
 	private HashMap<String, String> keyMap = new HashMap<>();
