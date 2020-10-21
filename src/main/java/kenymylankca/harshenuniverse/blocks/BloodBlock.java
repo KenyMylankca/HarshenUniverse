c67e2b3421357777c7c5761cab65dd521978c9f2
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2020-10-21 17:59:56 +0300

    removed tooltip of blood block

--- src/main/java/kenymylankca/harshenuniverse/blocks/BloodBlock.java
+++ src/main/java/kenymylankca/harshenuniverse/blocks/BloodBlock.java
@@ -8,15 +8,12 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -112,12 +109,5 @@ public class BloodBlock extends Block
 	public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos)
 	{
 		return true;
-	}
-	
-	@Override
-	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		tooltip.add("\u00A73" + new TextComponentTranslation("blood1").getFormattedText());
-		tooltip.add("\u00A73" + new TextComponentTranslation("blood2").getFormattedText());
-		super.addInformation(stack, worldIn, tooltip, flagIn);
 	}
 }
