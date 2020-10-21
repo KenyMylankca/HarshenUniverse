c683ccf6ed204d4414cf6f9bd645b28b18e48d20
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2020-10-21 14:36:50 +0300

    removed blood collector fills in offhand feature

--- src/main/java/kenymylankca/harshenuniverse/handlers/HandlerBloodOnHurt.java
+++ src/main/java/kenymylankca/harshenuniverse/handlers/HandlerBloodOnHurt.java
@@ -1,12 +1,8 @@
 package kenymylankca.harshenuniverse.handlers;
 
-import kenymylankca.harshenuniverse.HarshenItems;
 import kenymylankca.harshenuniverse.HarshenUtils;
 import kenymylankca.harshenuniverse.damagesource.DamageSourceBleeding;
-import kenymylankca.harshenuniverse.items.BloodCollector;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EntityDamageSource;
-import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -17,8 +13,5 @@ public class HandlerBloodOnHurt
 	{
 		if(event.getSource() instanceof EntityDamageSource || event.getSource() instanceof DamageSourceBleeding)
 			HarshenUtils.splashBlood(event.getEntityLiving());
-			
-		if(event.getSource().getTrueSource() instanceof EntityPlayer && ((EntityPlayer)event.getSource().getTrueSource()).getHeldItemOffhand().getItem() == HarshenItems.BLOOD_COLLECTOR)
-			((BloodCollector)((EntityPlayer)event.getSource().getTrueSource()).getHeldItemOffhand().getItem()).fill(((EntityPlayer)event.getSource().getTrueSource()), EnumHand.OFF_HAND, 1);
 	}
 }
