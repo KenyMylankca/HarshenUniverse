4cb80860db2db17c189958dbae39af9afb54f37a
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2020-10-24 01:11:34 +0300

    1.1.2

--- src/main/java/kenymylankca/harshenuniverse/HarshenUniverse.java
+++ src/main/java/kenymylankca/harshenuniverse/HarshenUniverse.java
@@ -29,7 +29,7 @@ public class HarshenUniverse
 {
     public static final String MODID = "harshenuniverse";
     public static final String MODNAME = "Harshen Universe";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
     
     public static boolean hasLoaded;
 
