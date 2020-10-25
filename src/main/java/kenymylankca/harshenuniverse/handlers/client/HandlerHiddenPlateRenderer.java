9141e55b305ba168ada602fb8f543bf3b1800c7f
KenyMylankca <ahmet.yilmaz1995@windowslive.com>
2020-10-26 00:07:57 +0300

    1.2.0 as release

--- src/main/java/kenymylankca/harshenuniverse/handlers/client/HandlerHiddenPlateRenderer.java
+++ src/main/java/kenymylankca/harshenuniverse/handlers/client/HandlerHiddenPlateRenderer.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class HandlerHiddenPlateRenderer 
+public class HandlerHiddenPlateRenderer
 {
 	private static ArrayList<BlockPos> platePositions = new ArrayList<BlockPos>();
 	
